3b65908637fbf31d7800123ec7b13c140d94910c
pochoco <pochoco248@gmail.com>
2025-09-17 21:28:29 -0400

    removing ledge grabbing because it may conflict with the game concept

--- player/states/falling.gd
+++ player/states/falling.gd
@@ -1,18 +1,5 @@
 extends BipedalState
 
-var enable_ledge_detection_signal = false
-
-# How far the character will be from the ledge while grabbing it
-@export var ledge_grab_distance: float
-
-
-func enter():
-	enable_ledge_detection_signal = true
-
-
-func exit():
-	enable_ledge_detection_signal = false
-
 
 func input_update(event: InputEvent):
 	if event.is_action_pressed("jump"):
@@ -34,9 +21,3 @@ func physics_update(delta: float):
 			change_state("Run")
 		else:
 			change_state("Idle")
-
-
-func _on_ledge_detection_ledge_detected(point: Vector3, normal: Vector3) -> void:
-	if enable_ledge_detection_signal:
-		player.rotation.y = normal.angle_to(Vector3.FORWARD)
-		change_state("LedgeGrab")
--- player/states/ledge_grab.gd
+++
@@ -1,8 +0,0 @@
-extends PlayerState
-
-
-func input_update(event: InputEvent) -> void:
-	if event.is_action_pressed("jump"):
-		change_state("Jumping")
-	else:
-		super(event)
