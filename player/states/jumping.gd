cc098a60f5522b83ea72602396f3adb3d9290cc8
pochoco <pochoco248@gmail.com>
2025-09-25 14:01:37 -0400

    fixed double jump bug

--- player/states/jumping.gd
+++ player/states/jumping.gd
@@ -14,7 +14,13 @@ func physics_update(delta: float):
 	
 	if player.velocity.y < 0.0:
 		change_state("Falling")
-	elif player.is_on_floor():
+	elif player.is_on_floor() and not player.velocity.y > 0.0:
+		# Check if player is on floor AND that player isn't already jumping
+		# this avoids double jump when the player exits ball and is clipping a bit
+		# through the floor.
+		# If you intend to add moving platforms that move up in the future
+		# you may want to change this approach.
+		
 		if not player.jump_buffer.is_stopped():
 			change_state("Jumping")
 		elif Input.get_vector("down", "up", "right", "left") != Vector2.ZERO:
